5cb3173a73901ccea43f244e0912f8122448e20a
Felix Mauch <mauch@fzi.de>
2016-04-12 09:26:17 +0200

    added an environment configuration possibility.
    Using this config file, one can specify which packages should be initially
    checked out in the workspaces.

--- robot_folders.py
+++ robot_folders.py
@@ -17,22 +17,13 @@ def cli():
     """A simple command line tool."""
 
 @cli.command('add_environment', short_help='Add a new environment')
-@click.option('--create_ic', is_flag=True, default=True,
-              prompt="Would you like to create an ic_workspace?",
-              help='Create an ic_workspace?')
-@click.option('--create_catkin', is_flag=True, default=True,
-              prompt="Would you like to create a catkin_workspace?",
-              help='Will not create a catkin_workspace.')
-@click.option('--create_mca2', is_flag=True, default=False,
-              prompt="Would you like to create an mca2_workspace?",
-              help='Will not create an mca2_workspace.')
-
-@click.option('--use_ninja', is_flag=True, default=False,
-              help='Use ninja as build system instead of linux makefiles.')
+@click.option('--generator', type=click.Choice(['ninja', 'makefiles']),
+              default='ninja',
+              help='Which generator should be used in this environment? Defaults to ninja.')
+@click.option('--config_file', help='Create an environment from a given config file.')
 @click.argument('env_name', nargs=1)
-@click.argument('config_file', nargs=1)
 #@click.option('-m', '--mca2_workspace', default=False, help='Create an mac2_workspace.')
-def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, config_file):
+def add_environment(generator, env_name, config_file):
     """Adds a new environment and creates the basic needed folders, e.g. a ic_orkspace and a catkin_workspace."""
     # TODO:
     #     - Add support for building in no_backup
@@ -40,27 +31,74 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
 
     base_dir = get_base_dir()
     ic_repo_url = "git://idsgit.fzi.de/core/ic_workspace.git"
-    ic_packages = ""
+    ic_packages = "base"
     ic_cmake_flags = ""
     cama_flags = ""
     catkin_rosinstall = ""
     mca_repo_url = "git://idsgit.fzi.de/mca2/mca2.git"
     mca_cmake_flags = ""
     mca_additional_repos = ""
+    build_cmd = "make"
+
+    create_ic = False
+    create_catkin = False
+    create_mca = False
 
     if config_file:
         yaml_stream = file(config_file, 'r')
         data = yaml_load(yaml_stream, Loader=Loader)
+        click.echo(data)
 
-        ic_packages = ' '.join(data['ic_workspace']['packages'])
-        catkin_rosinstall = data['catkin_workspace']['rosinstall']
-        mca_additional_repos = data['mca_workspace']
+        # Parse ic_workspace packages with error checking
+        if 'ic_workspace' in data:
+            create_ic = True
+            if data['ic_workspace'] is not None and 'packages' in data['ic_workspace']:
+                if data['ic_workspace']['packages'] is not None:
+                    ic_packages = ' '.join(data['ic_workspace']['packages'])
 
-    if use_ninja:
+
+        if 'catkin_workspace' in data:
+            create_catkin = True
+            if data['catkin_workspace'] is not None and 'rosinstall' in data['catkin_workspace']:
+                if data['catkin_workspace']['rosinstall'] is not None:
+                    catkin_rosinstall = data['catkin_workspace']['rosinstall']
+
+        if 'mca_workspace' in data:
+            create_mca = True
+            if data['mca_workspace'] is not None:
+                mca_additional_repos = data['mca_workspace']
+    else:
+        create_ic = click.confirm("Would you like to create an ic_workspace?", default=True)
+        create_catkin = click.confirm("Would you like to create a catkin_workspace?", default=True)
+        create_mca = click.confirm("Would you like to create an mca_workspace?", default=True)
+
+    # If we create a catkin_workspace query some more stuff at the beginning of the script.
+    if create_catkin:
+        installed_ros_distros = os.listdir("/opt/ros")
+        click.echo("Available ROS distributions: {}".format(installed_ros_distros))
+        ros_distro = installed_ros_distros[0]
+        if len(installed_ros_distros) > 1:
+            ros_distro = click.prompt('Which ROS distribution would you like to use?',
+                                      type=click.Choice(installed_ros_distros),
+                                      default=installed_ros_distros[0])
+        click.echo("Using ROS distribution \'{}\'".format(ros_distro))
+        ros_global_dir = "/opt/ros/{}".format(ros_distro)
+        copy_cmake_lists = click.confirm(("Would you like to copy the top-level CMakeLists.txt to the catkin"
+                                        " src directory instead of using a symlink?\n"
+                                        "(This is incredibly useful when using the QtCreator.)"),
+                                        default=True)
+
+
+
+
+
+
+    if generator == 'ninja':
         sep = " "
         ic_cmake_flags = sep.join([ic_cmake_flags, "-GNinja"])
         cama_flags = sep.join([cama_flags, "--use-ninja"])
         mca_cmake_flags = sep.join([mca_cmake_flags, "-GNinja"])
+        build_cmd = "ninja"
 
 
     if os.path.exists("{}/checkout/{}".format(base_dir, env_name)):
@@ -71,7 +109,7 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
     os.mkdir("{}/checkout/{}".format(base_dir, env_name))
     ic_directory = os.path.join(base_dir, "checkout", env_name, "ic_workspace")
     catkin_directory = os.path.join(base_dir, "checkout", env_name, "catkin_workspace")
-    mca_directory = os.path.join(base_dir, "checkout", env_name, "mca2_workspace")
+    mca_directory = os.path.join(base_dir, "checkout", env_name, "mca_workspace")
 
     env_source_file = open(os.path.join(base_dir, "checkout", env_name, "source_local.sh"), 'w')
 
@@ -97,6 +135,9 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
             process = subprocess.Popen(["bash", "-c", ic_cmake_cmd],
                                        cwd=ic_build_dir)
             process.wait()
+            process = subprocess.Popen(["bash", "-c", build_cmd],
+                                       cwd=ic_build_dir)
+            process.wait()
 
         except subprocess.CalledProcessError as e:
             click.echo(e.output)
@@ -110,20 +151,6 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
     # Check if we should create a catkin workspace and create one if desired
     if create_catkin:
         click.echo("Creating catkin_workspace")
-        installed_ros_distros = os.listdir("/opt/ros")
-        click.echo("Available ROS distributions: {}".format(installed_ros_distros))
-        ros_distro = installed_ros_distros[0]
-        if len(installed_ros_distros) > 1:
-            ros_distro = click.prompt('Which ROS distribution would you like to use?',
-                                      type=click.Choice(installed_ros_distros), default=installed_ros_distros[0])
-        click.echo("Using ROS distribution \'{}\'".format(ros_distro))
-        ros_global_dir = "/opt/ros/{}".format(ros_distro)
-        copy_cmake_lists = click.prompt(("Would you like to copy the top-level CMakeLists.txt to the catkin"
-                                        " src directory instead of using a symlink?"
-                                        " (This is incredibly useful when using the QtCreator.)"),
-                                        type=bool,
-                                        default=True)
-
 
         os.mkdir(catkin_directory)
         if catkin_rosinstall == "":
@@ -158,9 +185,9 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
 
 
 
-    # Check if we should create an mca2 workspace and create one if desired
-    if create_mca2:
-        click.echo("Creating mca2_workspace")
+    # Check if we should create an mca workspace and create one if desired
+    if create_mca:
+        click.echo("Creating mca_workspace")
 
         try:
             subprocess.check_call(["git", "clone", mca_repo_url, mca_directory])
@@ -173,13 +200,20 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
                                        cwd=mca_directory)
             process.wait()
 
-            for project in mca_additional_repos['projects']:
-                click.echo("Project: {}".format(project))
-                projects_dir = os.path.join(mca_directory, 'projects', project['git']['local-name'])
-                subprocess.check_call(["git", "clone", project['git']['uri'], projects_dir])
-            for library in mca_additional_repos['libraries']:
-                libraries_dir = os.path.join(mca_directory, 'libraries', library['git']['local-name'])
-                subprocess.check_call(["git", "clone", library['git']['uri'], libraries_dir])
+            if 'projects' in mca_additional_repos and mca_additional_repos['projects'] is not None:
+                for project in mca_additional_repos['projects']:
+                    click.echo("Project: {}".format(project))
+                    projects_dir = os.path.join(mca_directory,
+                                                'projects',
+                                                project['git']['local-name'])
+                    subprocess.check_call(["git", "clone", project['git']['uri'], projects_dir])
+
+            if 'libraries' in mca_additional_repos and mca_additional_repos['libraries'] is not None:
+                for library in mca_additional_repos['libraries']:
+                    libraries_dir = os.path.join(mca_directory,
+                                                 'libraries',
+                                                 library['git']['local-name'])
+                    subprocess.check_call(["git", "clone", library['git']['uri'], libraries_dir])
 
             # TODO: Do correct building here
             mca_build_dir = os.path.join(mca_directory, "build")
@@ -188,13 +222,16 @@ def add_environment(create_ic, create_catkin, create_mca2, use_ninja, env_name, 
             process = subprocess.Popen(["bash", "-c", mca_cmake_cmd],
                                        cwd=mca_build_dir)
             process.wait()
+            process = subprocess.Popen(["bash", "-c", build_cmd],
+                                       cwd=mca_build_dir)
+            process.wait()
 
         except subprocess.CalledProcessError as e:
             click.echo(e.output)
             click.echo("An error occurred while creating the ic_workspace. Exiting now")
             return
     else:
-        click.echo("Requested to not create an mca2 workspace")
+        click.echo("Requested to not create an mca workspace")
 
 
         click.echo("Initial workspace setup completed")
@@ -230,8 +267,8 @@ class EnvironmentChooser(click.MultiCommand):
         else:
             click.echo('No environment with name < %s > found.' % name)
             return None
-    
-      
+
+
 @cli.command('change_environment', cls=EnvironmentChooser, short_help='Add a new environment', invoke_without_command=True)
 @click.pass_context
 def change_environment(ctx):
