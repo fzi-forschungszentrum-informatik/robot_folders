aeb0bb0def9ec73595e53729db3c11d206c824bc
Felix Mauch <mauch@fzi.de>
2016-05-04 16:29:52 +0200

    added ce-completion for bash

--- bin/rob_folders-complete.sh
+++ bin/rob_folders-complete.sh
@@ -7,3 +7,18 @@ _rob_folders_completion() {
 
 complete -F _rob_folders_completion -o default fzirob;
 complete -F _rob_folders_completion -o default rob_folders;
+
+# ce completion in zsh works through the alias definition. For bash we have to declare it.
+if [ -n "${BASH_VERSION+1}" ];
+then
+    _ce_completion() {
+        _envs=$(ls ${ROB_FOLDERS_BASE_DIR}/checkout)
+        local cur prev
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        COMPREPLY=( $(compgen -W "${_envs}" -- ${cur}) )
+        return 0
+    }
+
+    complete -F _ce_completion -o default ce;
+fi
