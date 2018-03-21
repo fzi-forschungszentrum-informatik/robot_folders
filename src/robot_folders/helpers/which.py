b066b319d1fc256ef9f18ae1e2483b9179006186
Tobias Fleck <tfleck@fzi.de>
2018-03-21 21:50:48 +0100

    Fix which command in python.
    With the previous implementation only checks if the result of the split is not empty, which is only the case when program is empty.
    Now the base path gets checked and if only an executable name is given the PATH environment variable gets checked instead.

--- src/robot_folders/helpers/which.py
+++ src/robot_folders/helpers/which.py
@@ -9,9 +9,15 @@ def which(program):
         """Checks whether a path points to an executable file"""
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
-    for os_path in os.environ["PATH"].split(os.pathsep):
-        path = os_path + os.sep + program
-        if is_exe(path):
-          return program
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
 
     return None
