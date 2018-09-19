3aa87d14b3b5833da1758ab76a6c07a9c50e583e
Felix Mauch <mauch@fzi.de>
2018-09-19 11:28:54 +0200

    make default exit code 1

--- src/robot_folders/helpers/exceptions.py
+++ src/robot_folders/helpers/exceptions.py
@@ -2,7 +2,7 @@
 
 class ModuleException(Exception):
     """Generic exception in one command module"""
-    def __init__(self, message, module_name, return_code=0):
+    def __init__(self, message, module_name, return_code=1):
         super(ModuleException, self).__init__(message)
         self.message = message
         self.module_name = module_name
