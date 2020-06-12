2d4ed323de88ae72503ea955023f75d4dd693319
Felix Exner <exner@fzi.de>
2020-06-12 08:22:55 +0200

    compare against system's "which"

--- src/robot_folders/helpers/test_which.py
+++ src/robot_folders/helpers/test_which.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 
 from helpers import which
 
@@ -6,11 +7,11 @@ def test_is_exe():
     assert which.is_exe(sys.executable)
 
 def test_which_python():
-    assert which.which('python') == sys.executable
+    system_call = subprocess.run(["which", "python3"], capture_output=True)
+    assert which.which('python3') == system_call.stdout.strip().decode("utf-8")
 
 def test_which_interpreter():
     assert which.which(sys.executable) == sys.executable
 
 def test_which_none():
-    assert which.which('I_dont exist') == None
-
+    assert which.which('I_dont exist') is None
