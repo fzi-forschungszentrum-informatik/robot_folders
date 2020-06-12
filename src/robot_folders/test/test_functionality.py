66a721b12311ca4c3d573ba219bbf17a37fb1482
Felix Exner <exner@fzi.de>
2020-06-12 12:01:18 +0200

    added test folder and implemented first function test

---
+++ src/robot_folders/test/test_functionality.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import unittest
+
+import os
+
+import commands.active_environment
+import helpers.config_helpers as config_helpers
+
+class TestCLI(unittest.TestCase):
+    def test_rob_folders_runs(self):
+        try:
+            subprocess.run(["rob_folders"], stdout=subprocess.PIPE)
+        except:
+            (etype, evalue, etrace) = sys.exc_info()
+            self.fail("Failed with %s" % evalue)
+
+    def test_get_checkout_base_dir(self):
+        try:
+            process_result = subprocess.run(["rob_folders", "get_checkout_base_dir"],
+                                            stdout=subprocess.PIPE)
+        except:
+            (etype, evalue, etrace) = sys.exc_info()
+            self.fail("Failed with %s" % evalue)
+
+        expected = config_helpers.get_value_safe("directories", "checkout_dir")
+
+        if expected:
+            self.assertEqual(process_result.stdout.strip(), bytes(expected, 'utf-8'))
+        else:
+            self.assertEqual(process_result.stdout.strip(),
+                             os.path.expanduser(b"~/robot_folders/checkout"))
+
--- src/robot_folders/test/test_which.py
+++ src/robot_folders/test/test_which.py
@@ -7,7 +7,7 @@ def test_is_exe():
     assert which.is_exe(sys.executable)
 
 def test_which_python():
-    system_call = subprocess.run(["which", "python3"], capture_output=True)
+    system_call = subprocess.run(["which", "python3"], stdout=subprocess.PIPE)
     assert which.which('python3') == system_call.stdout.strip().decode("utf-8")
 
 def test_which_interpreter():
