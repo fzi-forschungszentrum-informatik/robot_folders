e53a7f11a876083475557027929bbd7133439920
Felix Exner <exner@fzi.de>
2020-07-08 18:14:41 +0200

    added (disabled) mca test
    For now, this is commented out as it didn't work out of the box.
    I don't want to put much time into that as I'd like to get rid of that
    at some point anyway.

--- src/robot_folders/test/test_functionality.py
+++ src/robot_folders/test/test_functionality.py
@@ -93,3 +93,33 @@ class TestCLI(unittest.TestCase):
         except:
             (etype, evalue, etrace) = sys.exc_info()
             self.fail("Failed with %s" % evalue)
+
+    # def test_add_mca_only(self):
+        # try:
+            # process_result = subprocess.check_call(
+            # ["rob_folders",
+            # "add_environment",
+            # "--create_ic=no",
+            # "--create_mca=yes",
+            # "--create_catkin=no",
+            # "--copy_cmake_lists=no",
+            # "testing_ws"])
+        # except:
+            # (etype, evalue, etrace) = sys.exc_info()
+            # self.fail("Failed with %s" % evalue)
+
+        # mca_dir = os.path.join(directory_helpers.get_checkout_dir(), "testing_ws", "mca_workspace")
+
+        # self.assertTrue(os.path.isdir(mca_dir))
+        # self.assertTrue(os.path.exists(os.path.join(mca_dir, "CMakeLists.txt")))
+
+        # # cleanup
+        # try:
+            # process_result = subprocess.check_call(
+            # ["rob_folders",
+            # "delete_environment",
+            # "--force",
+            # "testing_ws"])
+        # except:
+            # (etype, evalue, etrace) = sys.exc_info()
+            # self.fail("Failed with %s" % evalue)
